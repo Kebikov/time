c0b6bb8d44cb35e44b671f71f5bead2e83189bea
Kebikov Evgenii <kebikovgen@gmail.com>
2024-06-21 20:49:04 +0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React, { FC } from 'react';
+
+/**
+ * @component
+ * @example 
+ * @returns {JSX.Element}
+ */
+const index: FC = () => {
+
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default index;
